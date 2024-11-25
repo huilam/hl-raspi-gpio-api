cd698b0eef88cd1cebd2da0066e271a12a74b1db
onghuilam <onghuilam@gmail.com>
2024-11-25 13:30:18 +0800

    init commit

--- src/main/java/hl/raspi/gpio/PiGpio.java
+++ src/main/java/hl/raspi/gpio/PiGpio.java
@@ -1,9 +1,23 @@
 package hl.raspi.gpio;
 
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalOutputConfigBuilder;
+
 public class PiGpio{
+	
+	private static final int PIN_LED = 22;
 	
 	public static void main(String args[])
 	{
+		Context pi4j = Pi4J.newAutoContext();
 		
+		DigitalOutputConfigBuilder ledConfig = DigitalOutput.newConfigBuilder(pi4j)
+                .address(PIN_LED);
+		
+		DigitalOutput led = pi4j.create(ledConfig);
+		
+		led.on();
 	}
 }
